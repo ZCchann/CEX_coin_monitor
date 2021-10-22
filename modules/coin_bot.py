016b0f3fb40e06cd2e689f6439184753c1a36bfe
ZCchann <z2023234@gmail.com>
2021-10-22 22:10:46 +0800

    bot增加makedown解析

--- modules/coin_bot.py
+++ modules/coin_bot.py
@@ -10,7 +10,8 @@ def send_message(message):
     }
     data = {
         "chat_id": config.chat_id,  # 对话id。若是频道则为“@channel_id”
-        "text": str(message)  # 发送的信息正文。可以使用有限的几种 Markdown/HTML 进行标记
+        "text": str(message),  # 发送的信息正文。可以使用有限的几种 Markdown/HTML 进行标记
+        "parse_mode": "MarkdownV2"  # 使用markdown解析传送消息
     }
 
     requests.post(url=url, data=json.dumps(data), headers=headers)
